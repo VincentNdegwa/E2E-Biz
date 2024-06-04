414e118f4aa41d50db5af3e432176b209dc38d98
VincentNdegwa <ndegwavincent7@gmail.com>
2024-06-04 13:00:34 +0300

    retested

---
+++ cypress/e2e/data/product.js
@@ -0,0 +1,26 @@
+const product = {
+    name: "Auto product",
+    description: "Test description",
+    category: "Test category",
+    brand: "Test brand",
+    location: "Test location",
+    barcode: "123456789012",
+    opening_quantity: "100",
+    quantity_unit_code: "pieces",
+    buying_price: "50.00",
+    retail_price: "70.00"
+};
+const productEdit = {
+    name: "Auto product edited",
+    description: "Test description edited",
+    category: "Test category edit",
+    brand: "Test brand edit",
+    location: "Test location edit",
+    barcode: "6789",
+    opening_quantity: "100",
+    quantity_unit_code: "pieces",
+    buying_price: "40.00",
+    retail_price: "80.00"
+};
+
+export default { product, productEdit }
