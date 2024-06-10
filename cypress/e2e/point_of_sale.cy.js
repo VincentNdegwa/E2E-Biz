86ce1b0a1ea7647486e10872949a047a7bafcc3d
VincentNdegwa <ndegwavincent7@gmail.com>
2024-06-10 16:48:01 +0300

    creation of transaction

--- cypress/e2e/point_of_sale.cy.js
+++ cypress/e2e/point_of_sale.cy.js
@@ -31,10 +31,32 @@ describe("Performing a POS transaction", () => {
     cy.findByRole("heading", { name: /new order bill/i }).should("exist");
     cy.findByRole("button", { name: /proceed to checkout/i }).click();
     cy.findByRole("heading", { name: /mpesa/i }).parent().click();
-    // cy.findByRole("button", { name: /checkout/i });
+    // cy.findByRole("button", { name: /checkout/i }).click();
     cy.visit(link + "/dashboard/transactions");
-    cy.get('body').click()
-    cy.get(".MuiTableRow-root.MuiTableRow-hover.css-rw4e8j").eq(0);
+    cy.wait(1000);
+    cy.get("body").click();
+    cy.get(".MuiTableRow-root.MuiTableRow-hover.css-rw4e8j")
+      .eq(0)
+      .within(() => {
+        let elements = {
+          customer: "Walk In",
+          description: "Point of Sale",
+          category: "Pos",
+          net_amount: `UG ${ProductPos.retail_price}`,
+          vat_amount: "UG 0.00",
+          total_amount: `UG ${ProductPos.retail_price}`,
+          payment_type: "Mpesa",
+        };
+        let startIndex = 2;
+        for (let key in elements) {
+          cy.get("td")
+            .eq(startIndex)
+            .invoke("text")
+            .should("equal", elements[key]);
+          startIndex++;
+        }
+      });
+
     // Additional steps if needed, e.g., verifying the item is in the cart
   });
 
